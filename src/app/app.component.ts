9d2b708f2bcde03281202e56da9fbbc59844fecc
Jake Neal <hello@jakeneal.co.uk>
2024-06-12 11:50:41 +0100

    :lipstick: added cursor follow along

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/layout/header/header.component';
+import { gsap } from 'gsap';
 
 @Component({
     selector: 'app-root',
@@ -12,6 +13,66 @@ import { HeaderComponent } from './components/layout/header/header.component';
             <router-outlet></router-outlet>
         </main>
         <!-- <footer>footer goes here</footer> -->
+        <div class="cursor"></div>
     `,
+    styles: [
+        `
+            .cursor {
+                position: fixed;
+                left: 0;
+                border-radius: 50%;
+                pointer-events: none;
+                transition: transform 0.1s;
+                background: var(--brand-colour-tertiary);
+                top: 0;
+                width: 15px;
+                height: 15px;
+                z-index: 999;
+            }
+        `,
+    ],
 })
-export class AppComponent {}
+export class AppComponent {
+    ngOnInit() {
+        gsap.set('.cursor', {
+            xPercent: -50,
+            yPercent: -50,
+        });
+
+        const cursor = document.querySelector('.cursor');
+        const links = document.querySelectorAll('a');
+
+        let xTo = gsap.quickTo(cursor, 'x', {
+            duration: 0.3,
+            ease: 'power3',
+        });
+
+        let yTo = gsap.quickTo(cursor, 'y', {
+            duration: 0.3,
+            ease: 'power3',
+        });
+
+        // Initialise the follow along cursor on mousemove.
+        window.addEventListener('mousemove', (e) => {
+            xTo(e.clientX);
+            yTo(e.clientY);
+        });
+
+        // Loop through each link and add a mouseenter/mouseleave event listener to scale up/down the cursor size.
+        links.forEach((link) => {
+            link.addEventListener('mouseenter', () => {
+                gsap.to(cursor, 1, {
+                    scale: 1.6,
+                    duration: 0.1,
+                });
+            });
+
+            link.addEventListener('mouseleave', () => {
+                gsap.to(cursor, 1, {
+                    scale: 1,
+                    duration: 0.1,
+                });
+            });
+        });
+    }
+}
